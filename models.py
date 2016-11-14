573442444799a116b8c061a871bc02030b68a3e0
allox <alex.sd3@gmail.com>
2016-11-15 00:09:43 +0100

    fa icons added

--- models.py
+++ models.py
@@ -21,15 +21,17 @@ class Page(models.Model):
     title    = models.CharField(max_length=120, null=True)
     url      = models.CharField(max_length=120, null=True)
     template = models.CharField(max_length=120, null=True, choices=getTemplates())
-    content = RichTextField(config_name='awesome_ckeditor')
-    published  = models.BooleanField(default=False)
+    content  = RichTextField(config_name='awesome_ckeditor')
+    published= models.BooleanField(default=False)
     access   = models.CharField(max_length=120, null=True, default='public', choices=[('public','public'),('authenticated','authenticated')])
     author   = models.ForeignKey(User,null=True)
-    createdate  = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
-    updated     = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
+    createdate = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
+    updated  = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
 
     def __str__(self):
         return str(self.title)
 
     def get_absolute_url(self):
         return "/page/admin"
+
+
